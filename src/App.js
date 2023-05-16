c683dd3e87f8dc6c2a7be487544f2c252569e264
Feline Huhn <feline.huhn@gmx.de>
2023-05-16 16:00:54 +0200

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">My little Wiki</h2>
+      <label htmlFor="question">Today`s Question:</label>
+      <input id="question"></input>
+      <a className="article__link" href="www.wikipedia.com">
+        WIkipedia
+      </a>
+    </article>
+  );
 }
